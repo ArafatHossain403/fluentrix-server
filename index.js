50dd96e30ececaff39250ae4cdab1665173fee68
Arafat Hossain <arafatnubt@gmail.com>
2023-06-14 05:02:20 +0600

    jwt add

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
+
 
 
 app.use(cors());
@@ -33,6 +35,13 @@ async function run() {
         const coursesCartCollection = client.db("fluentrixDb").collection("coursesCart");
 
 
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+            res.send({ token })
+        })
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             const query = { email: user.email }
@@ -51,6 +60,8 @@ async function run() {
             const result = await usersCollection.find().toArray();
             res.send(result);
         })
+
+
         app.delete('/users/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
@@ -73,6 +84,20 @@ async function run() {
       
           })
 
+          app.patch('/users/instructor/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+              $set: {
+                role: 'instructor'
+              },
+            };
+      
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result);
+      
+          })
 
         app.get('/instructors', async (req, res) => {
             const result = await instructorsCollection.find().sort({ rating: -1 }).toArray();
